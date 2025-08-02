39d76a500ad4a3f1ea603abf93765a3f8506fdbe
piero-bit <jean.zambrano01@epn.edu.ec>
2025-08-01 21:13:39 -0500

    Clase Fondo y diagrama de clase creados

---
+++ TractorGroup4/Tractor/src/Fondo.java
@@ -0,0 +1,32 @@
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+
+public class Fondo {
+    Juego jueguito;
+    int anchoFondo=1300;
+    int altoFondo=400;
+    int x1=1300;
+    int y1=0;
+    int x2=0;
+    int y2=0;
+    
+    public Fondo(Juego jueguito){
+        this.jueguito=jueguito;
+    }
+    
+    public void mover(){
+        x1-=2;
+        x2-=2;
+        if(x1==0 && x2==-1300){
+            x1=1300;
+            x2=0;
+        }
+    }
+    
+    public void paint(Graphics2D g){
+        ImageIcon imagenFondo=new ImageIcon(getClass().getResource("/multimedia/fondo.jpg"));
+        g.drawImage(imagenFondo.getImage(),x1,y1,anchoFondo,altoFondo,null);
+        g.drawImage(imagenFondo.getImage(),x2,y2,anchoFondo,altoFondo,null);
+    }
+
+}
